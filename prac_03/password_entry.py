1ed261f3b10f5183eec269ec7d8e291aa5fc7dae
Cassandra King <cassandra.king4@my.jcu.edu.au>
2019-08-22 16:42:35 +1000

    remove function from inside other function

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,16 +1,14 @@
 """Cassandra King"""
 
-def main():
-    def get_password():
-        password = input("Password: ")
-        while len(password) <= 3:
-            print("Password should be longer then 3 characters.")
-            password = input("Password: ")
-        else:
-            return password
 
+def get_password():
+    password = input("Password: ")
+    while len(password) <= 3:
+        print("Password should be longer then 3 characters.")
+        password = input("Password: ")
+    else:
+        return password
+def main():
     password = get_password()
     print('*' * len(password))
-
-
 main()

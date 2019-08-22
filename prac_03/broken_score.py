8b81e4b2748c7edd787ec92248dd41c370fe5b8e
Cassandra King <cassandra.king4@my.jcu.edu.au>
2019-08-22 16:58:20 +1000

    Refactor score evaluator program and reorganised by highest score to lowest.

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,11 +1,15 @@
-score = float(input("Enter Score: "))
-if score < 0:
-    print("Invalid Score")
-elif score > 100:
-    print("Invalid Score")
-elif score < 50:
-    print("Bad")
-elif score >= 90:
-    print("Excellent")
-else:
-    print("Passable")
+
+def main():
+    score = float(input("Enter Score: "))
+    print(evalulate_status(score))
+
+def evalulate_status(score):
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    elif score >=90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+main()

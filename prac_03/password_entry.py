f4b817157a309daf261bbae6836c1fbab04c47f1
Cassandra King <cassandra.king4@my.jcu.edu.au>
2019-08-22 12:11:37 +1000

    Refactor password program to use functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,8 +1,13 @@
 """Cassandra King"""
+def main():
+    def get_password():
+        password = input("Password: ")
+        while len(password) <= 3:
+            print("Password should be longer then 3 characters.")
+            password = input("Password: ")
+        else:
+            return password
 
-password = input("Password: ")
-while len(password) <= 3:
-    print("Password should be longer then 3 characters.")
-    password = input("Password: ")
-else:
+    password = get_password()
     print('*' * len(password))
+main()

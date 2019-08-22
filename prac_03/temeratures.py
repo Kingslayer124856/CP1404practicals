6b68895a4faf6ba9691576bde6f9bce454055342
Cassandra King <cassandra.king4@my.jcu.edu.au>
2019-08-22 16:46:22 +1000

    remove function from inside other function

--- prac_03/temeratures.py
+++ prac_03/temeratures.py
@@ -1,3 +1,12 @@
+def celsius_to_fahrenheit():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+def fahreheit_to_celsius():
+    fahrenheit = float(input("Fahrenheit:"))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
 def main():
     MENU = """C - Convert Celsius to Fahrenheit
     F - Convert Fahrenheit to Celsius
@@ -6,17 +15,11 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            def celsius_to_fahrenheit():
-                celsius = float(input("Celsius: "))
-                fahrenheit = celsius * 9.0 / 5 + 32
-                return fahrenheit
             fahrenheit = celsius_to_fahrenheit()
             print("Result: {:.2f} F".format(fahrenheit))
         elif choice == "F":
-            def fahrenheit_to_celsius()
-            fahrenheit = float(input("Fahrenheit:"))
-            #celsius = 5 / 9 * (fahrenheit - 32)
-            #print("Result: {:.2f} C".format(celsius))
+            celsius = fahreheit_to_celsius()
+            print("Result: {:.2f} C".format(celsius))
         else:
             print("Invalid option")
         print(MENU)
